af7213ca626693cded29b4239269e33a9101524a
bllamond <vinaymalik1729@gmail.com>
2024-09-12 21:52:11 +0530

    timeout increased

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -1,14 +1,24 @@
 import { MongoClient } from 'mongodb';
 
-const uri = process.env.MONGODB_URI; 
-let client;
-let clientPromise;
+// Use environment variables for sensitive information
+const uri = process.env.MONGODB_URI;
 
-if (!process.env.MONGODB_URI) {
+if (!uri) {
   throw new Error('Please add your MongoDB URI to .env.local');
 }
 
-client = new MongoClient(uri);
+// Configure client options with timeouts
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  connectTimeoutMS: 30000, // 30 seconds
+  serverSelectionTimeoutMS: 30000 // 30 seconds
+};
+
+let client;
+let clientPromise;
+
+client = new MongoClient(uri, options);
 clientPromise = client.connect();
 
 export default clientPromise;
