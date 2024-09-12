dea22de0509268fba846b23f7a1e3b763aeead73
bllamond <vinaymalik1729@gmail.com>
2024-09-12 22:21:29 +0530

    Mongodb.js

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -1,15 +1,15 @@
-// import { MongoClient } from 'mongodb';
+import { MongoClient } from 'mongodb';
 
-// const uri = process.env.MONGODB_URI; 
-// let client;
-// let clientPromise;
+const uri = process.env.MONGODB_URI; 
+let client;
+let clientPromise;
 
-// if (!process.env.MONGODB_URI) {
-//   throw new Error('Please add your MongoDB URI to .env.local');
-// }
+if (!process.env.MONGODB_URI) {
+  throw new Error('Please add your MongoDB URI to .env.local');
+}
 
-// // Initialize MongoClient without deprecated options
-// client = new MongoClient(uri);
-// clientPromise = client.connect();
+// Initialize MongoClient without deprecated options
+client = new MongoClient(uri);
+clientPromise = client.connect();
 
-// export default clientPromise;
+export default clientPromise;
